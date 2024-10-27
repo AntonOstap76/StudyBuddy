3d301e1df3fca50d535786e51c242409d03dfec4
AntonOstap76 <antonostapchuk76@gmail.com>
2024-10-27 19:17:48 +0100

    chat room messages CRUD

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 
 from django.contrib.auth.models import User
 
@@ -111,7 +111,29 @@ def home(request):
 
 def room(request, pk):
     room=Room.objects.get(id=pk)
-    context={'room':room}
+
+#get all comments(messages)
+#giving all messages that are related to spesific room
+    room_messages=room.message_set.all().order_by('-created')
+
+    #adding participants
+    participants=room.participants.all()
+
+    if request.method == 'POST':
+        #creating a message by what written in the field 
+        message=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+
+        room.participants.add(request.user)
+
+        return redirect('room', pk=room.id)
+
+    context={'room':room,
+             'room_messages':room_messages,
+             'participants':participants}
     return render(request, 'base/room.html', context)
 
 # for creating a room 
@@ -169,3 +191,18 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render (request, 'base/delete.html', {'obj':room})
+
+#for deleting a message
+#restricted for user who is not login(redirected to login page)
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message=Message.objects.get(id=pk)
+
+#checking if the user who want to delete a room is the same who create it
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+
+    if request.method=='POST':
+        message.delete()
+        return redirect('home')
+    return render (request, 'base/delete.html', {'obj':message})
