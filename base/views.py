00dc29a461cffc7eb6f53529dd03a64c4f178df4
AntonOstap76 <antonostapchuk76@gmail.com>
2024-11-07 18:14:13 +0100

    mobile responsiveness

--- base/views.py
+++ base/views.py
@@ -98,7 +98,7 @@ def home(request):
     
 
     #get all topics 
-    topics=Topic.objects.all()
+    topics=Topic.objects.all()[0:5]
 
     room_count=rooms.count()
 
@@ -258,6 +258,18 @@ def updateUser(request):
              
 
 
-    return render(request, 'base/update-user.html', {'form':form,
-                                                     })
+    return render(request, 'base/update-user.html', {'form':form})
+
+#for mobile responsivness
+def topicsPage(request):
+    q=request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics=Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics':topics})
+
+#for mobile responsivness
+def activityPage(request):
+    room_messages=Message.objects.all()
+    return render(request, 'base/activity.html',{'room_messages':room_messages})
+
 
